533317c16d5111bdb2053bef205f45b3fcb8b78b
michaelperry101 <michael.perry94@outlook.com>
2025-08-26 23:53:47 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import "./globals.css";
 import dynamic from "next/dynamic";
-import { SidebarProvider } from "@/components/SidebarContext";
+import { SidebarProvider } from "../components/SidebarContext";
 
-const Header  = dynamic(()=>import("@/components/Header"),  { ssr:false });
-const Sidebar = dynamic(()=>import("@/components/Sidebar"), { ssr:false });
+const Header  = dynamic(() => import("../components/Header"),  { ssr: false });
+const Sidebar = dynamic(() => import("../components/Sidebar"), { ssr: false });
 
 export const metadata = { title: "HelpHub 24/7 – Carys" };
 
