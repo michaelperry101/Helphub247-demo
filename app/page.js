45aaa4f2622838044b092e703685dad9fe52756f
michaelperry101 <michael.perry94@outlook.com>
2025-08-26 20:58:00 +0100

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,2 @@
+'use client'
+export default function Signup(){ return (<div className="card" style={{maxWidth:420,margin:'0 auto'}}><h1>Create account</h1><input className="input" placeholder="Name" /><input className="input" placeholder="Email" /><input className="input" placeholder="Password" type="password" /><button className="btn primary">Create (Demo)</button></div>) }
